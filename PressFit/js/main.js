b638c701d8b0b23a8e9d4b5972edbbd202efc740
MichaelCodes <michael.lang3303@gmail.com>
2026-03-09 23:09:27 -0500

    Press Fit Inputs

---
+++ PressFit/js/main.js
@@ -0,0 +1,16 @@
+document.querySelector('#yell').addEventListener('click', run)
+
+function run() {
+  const pinOD = document.querySelector('#pinOD').value
+  const pinID = document.querySelector('#pinID').value
+  const pinLength = document.querySelector('#pinLength').value
+  const holeOD = document.querySelector('#holeOD').value
+  const holeID = document.querySelector('#holeID').value
+  const holeLength = document.querySelector('#holeLength').value
+
+  // document.querySelector('#placeToYell').innerText = fName + ' ' + fMidName + ' ' + ' ' + lMidName + ' ' + lName
+
+  //Add what you should be doing - conditionals go here
+
+  document.querySelector('#placeToYell').innerText = `${fName} ${fMidName} ${lMidName} ${lName}`
+}
