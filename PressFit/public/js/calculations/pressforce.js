438161aae163ff72ee8fce0a240b4e94b0e4c08e
MichaelCodes <michael.lang3303@gmail.com>
2026-03-14 08:24:59 -0500

    Press Force Function

--- PressFit/public/js/calculations/pressforce.js
+++ PressFit/public/js/calculations/pressforce.js
@@ -1,4 +1,7 @@
+import { calcPressure } from "./stress";
+
 export function calcAssemblyForce(imputs){
     const friction = 0.2;
-    
+    const pressure = calcPressure(inputs);
+    return (inputs.pinOD*Math.PI*pressure*friction*inputs.hubLength)
 }
