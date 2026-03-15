cc19eea71cbc3c3a935910b8bf5ee21d306bc56a
MichaelCodes <michael.lang3303@gmail.com>
2026-03-15 15:42:41 -0500

    Corrected Syntax Error

--- PressFit/public/js/calculations/pressforce.js
+++ PressFit/public/js/calculations/pressforce.js
@@ -1,6 +1,6 @@
 import { calcPressure } from "./stress";
 
-export function calcAssemblyForce(imputs){
+export function calcAssemblyForce(inputs){
     const friction = 0.2;
     const pressure = calcPressure(inputs);
     return (inputs.pinOD*Math.PI*pressure*friction*inputs.hubLength)
