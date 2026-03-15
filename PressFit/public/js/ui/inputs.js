935c3e76222f69ca349371e7394729d928974995
MichaelCodes <michael.lang3303@gmail.com>
2026-03-15 16:27:48 -0500

    Press Force Calc Working

--- PressFit/public/js/ui/inputs.js
+++ PressFit/public/js/ui/inputs.js
@@ -2,11 +2,13 @@ export function getInputs() {
   // Grab values from HTML inputs and convert to numbers
   const hubID = Number(document.querySelector("#hubID").value);
   const hubOD = Number(document.querySelector("#hubOD").value);
+  const hubLength = Number(document.querySelector("#hubLength").value);
   const hubStiffness = 200//Number(document.querySelector("#hubStiffness").value);
   const hubPois = 0.3//Number(document.querySelector("#hubPois").value);
 
   const pinID = Number(document.querySelector("#pinID").value);
   const pinOD = Number(document.querySelector("#pinOD").value);
+  const pinLength = Number(document.querySelector("#pinLength").value);
   const pinStiffness = 200//Number(document.querySelector("#pinStiffness").value);
   const pinPois = 0.3//Number(document.querySelector("#pinPois").value);
 
@@ -14,10 +16,12 @@ export function getInputs() {
   return {
     hubID,
     hubOD,
+    hubLength,
     hubStiffness,
     hubPois,
     pinID,
     pinOD,
+    pinLength,
     pinStiffness,
     pinPois
   };
