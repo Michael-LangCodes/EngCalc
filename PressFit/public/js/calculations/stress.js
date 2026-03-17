5786bf8a71c00e3e07d0c2bedbcc6e0d83391244
MichaelCodes <michael.lang3303@gmail.com>
2026-03-16 20:13:32 -0500

    Moved Deflection to Interface

--- PressFit/public/js/calculations/stress.js
+++ PressFit/public/js/calculations/stress.js
@@ -1,21 +1,4 @@
-import { interfere } from "./interface.js"
-
-// //Calculate Interference
-// export function interfere(inputs){
-//     return (inputs.pinOD / 2) - (inputs.hubID / 2);
-// }
-
-//Calculate Hub Deflection from Press
-export function hubDeflect(inputs){
-    return ((inputs.hubID / 2) / inputs.hubStiffness) *
-           ((((inputs.hubOD / 2) ** 2 + (inputs.hubID / 2) ** 2) / ((inputs.hubOD / 2) ** 2 - (inputs.hubID / 2) ** 2)) + inputs.hubPois);
-}
-
-//Calculate Pin OD Deflection from Press
-export function pinDeflect(inputs){
-    return ((inputs.pinOD / 2) / inputs.pinStiffness) *
-           ((((inputs.pinOD / 2) ** 2 + (inputs.pinID / 2) ** 2) / ((inputs.pinOD / 2) ** 2 - (inputs.pinID / 2) ** 2)) + inputs.pinPois);
-}
+import { interfere, hubDeflect, pinDeflect } from "./interface.js"
 
 // Calculate Interference Pressure
 export function calcPressure(inputs) {
