80f10bbb6b4a5248a631a2663c2048282e87fc5e
MichaelCodes <michael.lang3303@gmail.com>
2026-03-17 23:11:21 -0500

    Grabbing HubMat from Main.js

--- PressFit/public/js/main.js
+++ PressFit/public/js/main.js
@@ -26,8 +26,8 @@ document.querySelector('.button').addEventListener('click', ()=>{
   const pressForce = calcAssemblyForce(inputs);
   const pressure = calcPressure(inputs);
   const pressTorque = calcTorque(inputs);
-  console.log('this is the material data to follow')
   materialData('pinMat');
+  materialData('hubMat')
   //materialData('pinMat');
   const results = { hubStress, pinStress, pressForce, pressure, pressTorque };
   //console.log(results);
