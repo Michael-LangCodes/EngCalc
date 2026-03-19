4b5ecf8dc4ab9152352f8b67b5c9df68a4699f71
MichaelCodes <michael.lang3303@gmail.com>
2026-03-18 22:10:26 -0500

    Material Selection Color Resetting

--- PressFit/public/js/ui/matColor.js
+++ PressFit/public/js/ui/matColor.js
@@ -3,24 +3,27 @@ export function matSelectColor(id, button){
     const select = document.getElementById(id);
 
     // Start white
-    select.classList.add("normal");
+    select.classList.add("matNormal");
 
     // On submit click
     button.addEventListener("click", () => {
+        console.log('I have seen this click inside MatColor')
+        console.log(select.value)
         if (select.value === "none") {
-            select.classList.add("error");
-            select.classList.remove("normal");
+            select.classList.add("matError");
+            select.classList.remove("matNormal");
         } else {
-            select.classList.remove("error");
-            select.classList.add("normal");
+            select.classList.remove("matError");
+            select.classList.add("matNormal");
         }
     });
 
     // On change
     select.addEventListener("change", () => {
+        console.log('I have seen this change inside MatColor')
         if (select.value !== "none") {
-            select.classList.remove("error");
-            select.classList.add("normal");
+            select.classList.remove("matError");
+            select.classList.add("matNormal");
         }
     });
 }
