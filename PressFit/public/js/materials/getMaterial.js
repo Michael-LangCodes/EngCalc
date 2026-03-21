f7abd8cdb65dff8a436dca070420904ce24764a0
MichaelCodes <michael.lang3303@gmail.com>
2026-03-20 20:30:58 -0500

    Added Steel Material

--- PressFit/public/js/materials/getMaterial.js
+++ PressFit/public/js/materials/getMaterial.js
@@ -41,3 +41,26 @@ export async function aluminumData() {
     console.error('Error fetching JSON:', error);
   }
 }
+
+/**
+ * Steel Material Import
+ */
+export async function steelData() {
+  try {
+    const response = await fetch('./js/materials/materials.json'); // Fetch the file
+    if (!response.ok) {
+      throw new Error(`HTTP error! Status: ${response.status}`);
+    }
+    const jsonData = await response.json(); // Parse the response as JSON
+    console.log(jsonData.steel); // Use the data (it's a JS object)
+    console.log(jsonData.steel.YoungModulus)
+    let stiffness = jsonData.YoungModulus;
+    let pois = jsonData.PoissonRation;
+    return {
+         stiffness,
+         pois
+    }
+  } catch (error) {
+    console.error('Error fetching JSON:', error);
+  }
+}
